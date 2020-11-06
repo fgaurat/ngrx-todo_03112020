72b3c4294f13267edd2c396d8f6d35b24a9471e7
Frederic GAURAT <fgaurat@eolem.com>
2020-11-06 15:18:32 +0100

    add enum

--- src/app/todo/reducers/todo.reducer.ts
+++ src/app/todo/reducers/todo.reducer.ts
@@ -1,9 +1,15 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
+import { loadTodoSuccess } from '../actions/todo.actions';
 
 export const initialState = [];
 
 
-const _todoReducer = createReducer(initialState)
+const _todoReducer = createReducer(initialState,
+    on(loadTodoSuccess, (state, action) => {
+        let newState = action.todos
+        return newState
+    }),
+    )
 
 export function todoReducer(state, action) {
     return _todoReducer(state, action);
